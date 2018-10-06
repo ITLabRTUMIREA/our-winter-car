421e1159339c7c1c75405288a9324ce4b7e9255a
Nick <nikitamm59rus@gmail.com>
2018-10-06 12:13:12 +0300

    Added hints

---
+++ Assets/Scripts/HintLine.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.Serialization.Formatters;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(LineRenderer)), ExecuteInEditMode]
+public class HintLine : MonoBehaviour
+{
+	[Header("Set by yourself")]
+	public string HintText = "No hints applied";
+	public Text TextField;
+	[Header("Don't touch if you're not a developer")]
+	public Material material;
+	public Transform target;
+	private LineRenderer _lineRenderer;
+	
+	void Start ()
+	{
+		_lineRenderer = GetComponent<LineRenderer>();
+		_lineRenderer.positionCount = 2;
+		_lineRenderer.SetWidth(0.01f, 0.01f);
+		_lineRenderer.material = material;
+		////////////////////////////
+		TextField.text = HintText;
+	}
+	
+	void FixedUpdate () 
+	{
+		if (target == null)
+		{
+			return;
+		}
+		
+		_lineRenderer.SetPosition(0, transform.position);
+		_lineRenderer.SetPosition(1, target.position);
+	}
+}
